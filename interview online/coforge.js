fa061f31a06f4b54516211f5a01ace59204fa0c8
neetugond <93507242+neetugond@users.noreply.github.com>
2022-06-06 01:26:00 +0530

    coforge

---
+++ interview online/coforge.js
@@ -0,0 +1,24 @@
+var abc = 4 + "3" + 2 + 6
+console.log('abc:', abc) // output is 4326
+
+
+// // we can't intialize like this give error unexpected token
+// int i = 0; 
+// for(i < 5 ; i++){
+
+// }
+// console.log(i)
+
+
+// functional component in react
+function react() {
+    // hook at the top
+    return (
+        <>
+        </>
+   ) 
+}
+export default react
+
+
+
