d42fb7852400caf96523d3bd17a68be1a320f128
neetugond <93507242+neetugond@users.noreply.github.com>
2022-06-08 05:19:23 +0530

    hof practice

--- hof.js
+++ hof.js
@@ -76,3 +76,5 @@ console.log('cubeSum:', cubeSum)
 
 
 
+
+
--- hof_cb.js
+++ hof_cb.js
@@ -15,12 +15,51 @@ const subs = (a, b) => {
 const multi = (a, b) => {
     return a * b;
 }
-// hof - calculator
-// cbf - add, subs, multi
+
 const calculator = (num1, num2, operator) => {
     return operator(num1, num2)
 }
-
+// hof - calculator
+// cbf - add, subs, multi
 console.log(calculator(5, 2, add))
 console.log(calculator(5, 2, subs))
-console.log(calculator(5,2, multi))
+console.log(calculator(5, 2, multi))
+
+
+// practicing inbuilt hof function
+console.log("................")
+
+let arr = [6, 2, 3, 4, 5];
+
+const map =arr.map((ele, i, arr) => {
+
+return ele*2
+})
+console.log('map:', map)
+
+console.log("................")
+console.log("foreach")
+const foreach = arr.forEach((ele, i, arr) => {
+   
+    console.log(ele*3)
+})
+
+console.log("................")
+const filter = arr.filter((ele, i, arr) => {
+   
+    return ele%2==0
+})
+console.log('filter:', filter)
+
+console.log("................")
+
+const reduce = arr.reduce((cv, ac) => {
+    
+    return cv+ac
+})
+console.log('reduce:', reduce)
+
+
+
+
+
