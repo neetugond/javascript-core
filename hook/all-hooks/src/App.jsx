f8f4eb30f08dd71cfd163760bf51ab1a2ebbfb2c
neetugond <93507242+neetugond@users.noreply.github.com>
2022-06-26 23:04:43 +0530

    useState input

--- hook/all-hooks/src/App.jsx
+++ hook/all-hooks/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import UseStateHook from './components/UseStateHook'
+import UseStateHook2 from './components/UseStateHook2'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<UseStateHook></UseStateHook>}></Route>
+      <Route path='/useState' element={<UseStateHook2 />}></Route>
     </Routes>
   )
 }
---
+++ hook/all-hooks/src/components/UseStateHook2.jsx
@@ -0,0 +1,22 @@
+import React,{useState} from 'react'
+
+const UseStateHook2 = () => {
+    const [inputValue, setInputValue] = useState()
+
+    const inputText = (e) => {
+        const newInput = e.target.value 
+        setInputValue(newInput)
+    }
+  return (
+      <div>
+          <h1>useState hook input example</h1>
+          <input type="text" onChange={inputText} />
+          {/* <button onClick={inputText}>submit</button> */}
+          <h3>{ inputValue}</h3>
+      </div>
+      
+
+  )
+}
+
+export default UseStateHook2
